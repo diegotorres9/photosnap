5e6271fa11d1bc5e9a3cc4ee7256f4e175dc3c92
Diego Torres <diegoetorres1@gmail.com>
2026-05-26 17:58:15 -0400

    mobile overlay on menu open

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,17 @@ const menuIcon = document.querySelector(".menu-icon");
 console.log(menuIcon);
 const mobileMenu = document.querySelector(".mobile-menu");
 const closeMobileMenu = document.querySelector(".close-menu-icon");
+const overlay = document.querySelector(".overlay");
 
 menuIcon.addEventListener("click", () => {
     mobileMenu.classList.toggle("mobile-anim");
     closeMobileMenu.style.display = "block";
     menuIcon.style.display = "none";
+    overlay.style.height = "100%";
 })
 closeMobileMenu.addEventListener("click", () => {
     menuIcon.style.display = "block";
     closeMobileMenu.style.display = "none";
     mobileMenu.classList.toggle("mobile-anim");
+    overlay.style.height = "0";
 })
