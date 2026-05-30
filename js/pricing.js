14acc4d133b7a603bcb878bfc001d6754d9c3d2c
Diego Torres <diegoetorres1@gmail.com>
2026-05-30 08:15:36 -0400

    js for pricing switch toggle

--- js/pricing.js
+++ js/pricing.js
@@ -1,7 +1,29 @@
 const pricingSwitch = document.querySelector(".pricing-switch");
+let basicCost = document.querySelector(".basic-cost");
+let proCost = document.querySelector(".pro-cost");
+let businessCost = document.querySelector(".business-cost");
+let perTime = document.querySelectorAll(".per-time");
 
+// perTimeChange = perTime.map(time => time.textContent);
 
+console.log(perTime);
 
-pricingSwitch.addEventListener("click", () => {
+pricingSwitch.addEventListener("change", () => {
     // window.alert("Im toggled");
+    if (pricingSwitch.checked) {
+        basicCost.textContent = "190.00";
+        proCost.textContent = "390.00";
+        businessCost.textContent = "990.00";
+        perTime.forEach((time) => {
+            time.textContent = "per year";
+        });
+    } else {
+        basicCost.textContent = "19.00"
+        proCost.textContent = "39.00";
+        businessCost.textContent = "99.00";
+        perTime.forEach((time) => {
+            time.textContent = "per month";
+        });
+    }
+    
 });
